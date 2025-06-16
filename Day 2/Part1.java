6e101ad24c812e1d7e860e4420d0f7479eb718c1
Miranda Carroll <meatballsander@gmail.com>
2025-06-16 17:12:00 +0100

    made part 1 of day 2 more modular

--- Day 2/Part1.java
+++ Day 2/Part1.java
@@ -16,6 +16,9 @@ import java.lang.NumberFormatException;
 // if ever unsafe, terminate before finishing report and go to the next
 
 public class Part1 {
+    public int safeReports = 0;
+    public int totalReports = 0;   // I'm just curious
+
     public static void main(String[] args) {
         // 1 argument: file name
         if (args.length != 1) {
@@ -23,61 +26,30 @@ public class Part1 {
             return;
         }
 
+        Part1 Foo = new Part1();
+
         try {
             // read file, line by line (each line is one report)
             BufferedReader br = new BufferedReader(new FileReader(args[0]));
             String line;
-            int safeReports = 0;
-            int totalReports = 0;   // I'm just curious
 
             while ((line = br.readLine()) != null) {
-                totalReports++;
-
-                // determine whether report is safe
-                // if so, incremement total counter
+                Foo.totalReports++;
                 String[] report = line.split(" ");
-                int lastLevel = Integer.parseInt(report[0]);
-                int currLevel;
-                // need to flag whether a report is increasing or decreasing
-                int incrFlag = 0;
-                int decrFlag = 0;
-                boolean safe = true;
 
-                for (int i = 1; i < report.length; i++) {
-                    currLevel = Integer.parseInt(report[i]);
-
-                    // first, check step distance is safe
-                    int step = currLevel - lastLevel;
-                    if (step == 0 || Math.abs(step) > 3) {
-                        System.out.print("Not safe, ");
-                        safe = false;
-                        break;
-                    }
-
-                    // log if we're currently increasing or decreasing (for future checks)
-                    if (step < 0) { decrFlag++; }
-                    if (step > 0) { incrFlag++; }
-
-                    // secondly, check if this is increasing or decreasing
-                    // if we've done the opposite before, this isn't safe
-                    // (step is negative if we're decreasing, positive if increasing)
-                    if ((step > 0 && decrFlag > 0) || (step < 0 && incrFlag > 0)) {
-                        System.out.print("Not safe, ");
-                        safe = false;
-                        break;
-                    }
-
-                    // set lastLevel for next loop
-                    lastLevel = currLevel;
+                if (Foo.isReportSafe(report)) {
+                    System.out.print("S!, ");
+                    Foo.safeReports++;
+                } else {
+                    System.out.print("ns, ");
                 }
 
-                if (safe) {
-                    System.out.print("Safe!, ");
-                    safeReports++;
-                }
+                
             }  
             
-            System.out.println("\nTOTAL REPORTS: " + totalReports + "\nSAFE REPORTS: " + safeReports);
+            System.out.println("\nTOTAL REPORTS: " + Foo.totalReports + "\nSAFE REPORTS: " + Foo.safeReports);
+
+            br.close();
 
         } catch (FileNotFoundException e) {
             System.err.println("File not found exception: " + e.getMessage());
@@ -87,5 +59,46 @@ public class Part1 {
             System.err.println("Number format exception: " + e.getMessage());
         }
 
+    }
+
+
+
+    public boolean isReportSafe(String[] report) throws NumberFormatException {
+        // determine whether report is safe
+        // if so, incremement total counter
+        int lastLevel = Integer.parseInt(report[0]);
+        int currLevel;
+        // need to flag whether a report is increasing or decreasing
+        int incrFlag = 0;
+        int decrFlag = 0;
+        boolean safe = true;
+
+        for (int i = 1; i < report.length; i++) {
+            currLevel = Integer.parseInt(report[i]);
+
+            // first, check step distance is safe
+            int step = currLevel - lastLevel;
+            if (step == 0 || Math.abs(step) > 3) {
+                safe = false;
+                break;
+            }
+
+            // log if we're currently increasing or decreasing (for future checks)
+            if (step < 0) { decrFlag++; }
+            if (step > 0) { incrFlag++; }
+
+            // secondly, check if this is increasing or decreasing
+            // if we've done the opposite before, this isn't safe
+            // (step is negative if we're decreasing, positive if increasing)
+            if ((step > 0 && decrFlag > 0) || (step < 0 && incrFlag > 0)) {
+                safe = false;
+                break;
+            }
+
+            // set lastLevel for next loop
+            lastLevel = currLevel;
+        }
+
+        return safe;
     }
 }
